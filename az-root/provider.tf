ec92274147abf206a93f050b6306b88df704530e
williamcastelino <williamcastelino99@gmail.com>
2025-02-03 02:42:36 +0530

    Update provider.tf

--- az-root/provider.tf
+++ az-root/provider.tf
@@ -1,7 +1,3 @@
 provider "azurerm" {
-  client_id       = var.AZURE_CLIENT_ID
-  client_secret   = var.AZURE_CLIENT_SECRET
-  tenant_id       = var.AZURE_TENANT_ID
-  subscription_id = var.AZURE_SUBSCRIPTION_ID
   features         {}
 }
